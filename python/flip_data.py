bd5482c7c39c4e64d795605a56fe3dc8a21d269f
Nick Tiwari <ntiwari@andrew.cmu.edu>
2022-11-29 17:05:36 -0500

    Edited Python File to Flip Data

--- python/flip_data.py
+++ python/flip_data.py
@@ -2,6 +2,8 @@
 
 import sys,os
 import pandas as pd
+
+# Set this value to the location of Pizza Py Tools
 path = os.environ["LAMMPS_PYTHON_TOOLS"]
 sys.path.append(path)
 from data import data
@@ -14,10 +16,8 @@ box_mod = list(box)
 dat.headers["zlo zhi"]=tuple(box_mod)
 dat.headers["xlo xhi"]=box_z
 x_coords = dat.get("Atoms",5)
-#x_mod = [x+5 for x in x_coords]
 dat.replace("Atoms",5,x_coords)
 dat.reorder("Atoms",1,2,3,4,7,6,5)
 dat.write(sys.argv[1]+"_edit.data")
 
-#dat.write("nafion.data")
 
