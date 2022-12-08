d1cc21b847494c187e48973a7819836498dc41d4
Nick Tiwari <ntiwari@andrew.cmu.edu>
2022-12-08 13:52:33 -0500

    Running GCMD file, with additional NVE step

--- python/flip_data.py
+++ python/flip_data.py
@@ -3,6 +3,7 @@
 import sys,os
 import pandas as pd
 
+
 # Set this value to the location of Pizza Py Tools
 path = os.environ["LAMMPS_PYTHON_TOOLS"]
 sys.path.append(path)
@@ -18,6 +19,12 @@ dat.headers["xlo xhi"]=box_z
 x_coords = dat.get("Atoms",5)
 dat.replace("Atoms",5,x_coords)
 dat.reorder("Atoms",1,2,3,4,7,6,5)
+
 dat.write(sys.argv[1]+"_edit.data")
 
-
+if len(sys.argv) == 3:
+    reorder_dict = eval(sys.argv[2])
+    types = dat.get("Atoms",3)
+    types_modified = [reorder_dict[int(type)] for type in types]
+    dat.replace("Atoms",3,types_modified)
+    
